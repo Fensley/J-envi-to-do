4dd0319d19f52bedca2ba92ccf759a9f580edd45
Fensley <fortilusnehemie@gmail.com>
2024-06-21 16:21:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import myimage from "../src/data/Design tools-amico.png";
 import { useState } from "react";
 import logo from "../src/data/Humaaans - Plant 1.png";
 export default function App() {
@@ -8,90 +7,6 @@ export default function App() {
       <Section />
       <Footer />
     </>
-  );
-}
-function Footer() {
-  return (
-    <footer>
-      <p>
-        Made with <i class="fa-solid fa-heart" style={{ color: "red" }}></i>
-      </p>
-      <a href="https://github.com/fensley">
-        <i className="fa-brands fa-github"></i>
-      </a>
-    </footer>
-  );
-}
-
-function Section() {
-  return (
-    <section>
-      <Inputbar />
-      <Card />
-    </section>
-  );
-}
-function Inputbar() {
-  return (
-    <div className="input-ctn">
-      <input placeholder="add your to do" />
-      <div className="Add-ctn">
-        <button>Add</button>
-      </div>
-    </div>
-  );
-}
-function Card() {
-  return (
-    <div className="data">
-      <div className="bar1">
-        <p> Create my app API data before 6pm</p>
-        <div className="bar-icons">
-          <i class="fa-solid fa-trash"></i>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </div>
-      </div>
-
-      <div className="long-bar"></div>
-
-      <div className="bar1">
-        <p>Deploy App and then get into meeting with team </p>
-        <div className="bar-icons">
-          <i class="fa-solid fa-trash"></i>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </div>
-      </div>
-
-      <div className="long-bar"></div>
-
-      <div className="bar1">
-        <p>Go pick up pizza tonight</p>
-        <div className="bar-icons">
-          <i class="fa-solid fa-trash"></i>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </div>
-      </div>
-
-      <div className="long-bar"></div>
-
-      <div className="bar1">
-        <p>Going to soho house with my friends</p>
-        <div className="bar-icons">
-          <i class="fa-solid fa-trash"></i>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </div>
-      </div>
-
-      <div className="long-bar"></div>
-
-      <div className="bar1">
-        <p>Get prepare for an marathon</p>
-        <div className="bar-icons">
-          <i class="fa-solid fa-trash"></i>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </div>
-      </div>
-    </div>
   );
 }
 
@@ -125,5 +40,203 @@ function ImgLogo() {
     <div className="first-nav nav">
       <img src={logo} alt="logo" height={70} />
     </div>
+  );
+}
+
+function Section() {
+  const [message, setmessage] = useState("");
+  const [display, setDisplay] = useState("");
+  const [editable, seteditable] = useState(true);
+  // const [clear, setclear] = useState("");
+  function handlemessage(e) {
+    setmessage(e.target.value);
+  }
+
+  function handledisplay() {
+    setDisplay(message);
+    setmessage("");
+  }
+
+  function handleeditable() {
+    seteditable((not) => !not);
+  }
+
+  function handleclear() {
+    setDisplay("");
+  }
+
+  return (
+    <section>
+      <Inputbar
+        message={message}
+        handlemessage={handlemessage}
+        handledisplay={handledisplay}
+      />
+      <Card
+        message={message}
+        display={display}
+        editable={editable}
+        handleeditable={handleeditable}
+        handleclear={handleclear}
+      />
+    </section>
+  );
+}
+function Inputbar({ handlemessage, handledisplay, display, message }) {
+  return (
+    <div className="input-ctn">
+      <input
+        placeholder="add your to do"
+        onChange={handlemessage}
+        value={message}
+      />
+      <div className="Add-ctn">
+        <button onClick={handledisplay}>Add</button>
+      </div>
+    </div>
+  );
+}
+function Card({
+  message,
+  display,
+  editable,
+  handledisplay,
+  handleeditable,
+  handleclear,
+}) {
+  return (
+    <div className="data">
+      <Cardone
+        display={display}
+        editable={editable}
+        handleeditable={handleeditable}
+        handleclear={handleclear}
+      />
+      <Longbar />
+      <Cardtwo
+        message={message}
+        editable={editable}
+        handleeditable={handleeditable}
+      />
+      <Longbar />
+      <Cardthree
+        message={message}
+        editable={editable}
+        handleeditable={handleeditable}
+      />
+      <Longbar />
+      <Cardfour
+        message={message}
+        editable={editable}
+        handleeditable={handleeditable}
+      />
+      <Longbar />
+      <Cardfive
+        message={message}
+        editable={editable}
+        handleeditable={handleeditable}
+      />
+    </div>
+  );
+}
+
+function Cardone({ message, display, handleeditable, editable, handleclear }) {
+  return (
+    <div className="bar1">
+      <p
+        suppressContentEditableWarning={true}
+        contentEditable={editable ? false : true}
+      >
+        {" "}
+        {display}
+      </p>
+      <Theicons handleeditable={handleeditable} handleclear={handleclear} />
+    </div>
+  );
+}
+
+function Cardtwo({ handleeditable, editable }) {
+  return (
+    <div className="bar1">
+      <p
+        suppressContentEditableWarning={true}
+        contentEditable={editable ? false : true}
+      >
+        Deploy App and then get into meeting with team{" "}
+      </p>
+      <Theicons handleeditable={handleeditable} />
+    </div>
+  );
+}
+
+function Cardthree({ handleeditable, editable }) {
+  return (
+    <div className="bar1">
+      <p
+        suppressContentEditableWarning={true}
+        contentEditable={editable ? false : true}
+      >
+        Go pick up pizza tonight
+      </p>
+      <Theicons handleeditable={handleeditable} />
+    </div>
+  );
+}
+
+function Cardfour({ handleeditable, editable }) {
+  return (
+    <div className="bar1">
+      <p
+        suppressContentEditableWarning={true}
+        contentEditable={editable ? false : true}
+      >
+        Going to soho house with my friends
+      </p>
+      <Theicons handleeditable={handleeditable} />
+    </div>
+  );
+}
+
+function Cardfive({ handleeditable, editable }) {
+  return (
+    <div className="bar1">
+      <p
+        suppressContentEditableWarning={true}
+        contentEditable={editable ? false : true}
+      >
+        Get prepare for an marathon
+      </p>
+      <Theicons handleeditable={handleeditable} editable={editable} />
+    </div>
+  );
+}
+
+function Theicons({ handleeditable, editable, handleclear }) {
+  return (
+    <div className="bar-icons">
+      <i className="fa-solid fa-trash" onClick={handleclear}></i>
+      <i className="fa-solid fa-pen-to-square" onClick={handleeditable}></i>
+    </div>
+  );
+}
+
+function Longbar() {
+  return <div className="long-bar"></div>;
+}
+
+function Footer() {
+  return <Footerdata />;
+}
+
+function Footerdata() {
+  return (
+    <footer>
+      <p>
+        Made with <i className="fa-solid fa-heart" style={{ color: "red" }}></i>
+      </p>
+      <a href="https://github.com/fensley">
+        <i className="fa-brands fa-github"></i>
+      </a>
+    </footer>
   );
 }
